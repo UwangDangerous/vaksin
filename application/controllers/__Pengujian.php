424d22ebddd759820c81df71523e60fcc2e6aef4
ridwanhafif <ridwanhafidudin@gmail.com>
2022-04-14 07:53:48 +0700

    stop dulu

--- application/controllers/__Pengujian.php
+++ application/controllers/__Pengujian.php
@@ -44,7 +44,7 @@
 
             if($this->db->insert('hasil_pengujian', $query)){
                 $this->load->model('_Riwayat') ;
-                $this->_Riwayat->simpanRiwayat($idBatch, 'Pengujian '.$this->input->post('noAdm').' Selesai', 'Hasil Pengujian',1);
+                $this->_Riwayat->simpanRiwayat($idBatch, 'Pengujian '.$this->input->post('noAdm').' Selesai -  Oleh '. $this->session->userdata('nama'), 'Hasil Pengujian',1);
                 $pesan = [
                     'pesan' => 'Hasil Pengujian Berhasil Disimpan',
                     'warna' => 'success'
@@ -56,7 +56,55 @@
                 ];
             }
 
-            $this->session->set_flasdata($pesan) ;
+            $this->session->set_flashdata($pesan) ;
+            redirect('__pengujian') ;
+        }
+
+        public function ubah_hasil_pengujian($id, $idBatch) //id = id_hasil_pengujian
+        {
+
+            $this->load->model('_Upload') ;
+            if($_FILES['berkas']['name']) {
+                $query = [
+                    'uji_yg_dilakukan' => $this->input->post('uji_yg_dilakukan') ,
+                    'hasil' => $this->input->post('hasil') ,
+                    'syarat' => $this->input->post('syarat') ,
+                    'acuan' => $this->input->post('acuan') ,
+                    'tgl_kadaluarsa_sample' => $this->input->post('tgl_expiry_pengujian') ,
+                    'tgl_selesai_pengujian' => date("Y-m-d")
+                ];
+            }else{
+                $query = [
+                    'uji_yg_dilakukan' => $this->input->post('uji_yg_dilakukan') ,
+                    'hasil' => $this->input->post('hasil') ,
+                    'syarat' => $this->input->post('syarat') ,
+                    'acuan' => $this->input->post('acuan') ,
+                    'tgl_kadaluarsa_sample' => $this->input->post('tgl_expiry_pengujian') ,
+                    'tgl_selesai_pengujian' => date("Y-m-d"),
+                    'file_hasil_pengujian' => $this->_Upload->uploadEksUser('berkas', 'assets/file-upload/hasil-pengujian', 'pdf|doc|docx','__pengujian', 'hasil_pengujian')
+                ];
+            }
+
+            // var_dump($query) ; die ;
+
+            $this->db->where('id_hasil_pengujian', $id) ;
+            $this->db->set($query) ;
+
+            if($this->db->update('hasil_pengujian')){
+                $this->load->model('_Riwayat') ;
+                $this->_Riwayat->simpanRiwayat($idBatch, 'Pengujian '.$this->input->post('noAdm').' Diubah - Oleh '. $this->session->userdata('nama'), 'Hasil Pengujian',1);
+                $pesan = [
+                    'pesan' => 'Hasil Pengujian Berhasil Diubah',
+                    'warna' => 'success'
+                ];
+            }else{
+                $pesan = [
+                    'pesan' => 'Hasil Pengujian Gagal Diubah',
+                    'warna' => 'danger'
+                ];
+            }
+
+            $this->session->set_flashdata($pesan) ;
             redirect('__pengujian') ;
         }
     }
--- application/views/__pengujian/index.php
+++ application/views/__pengujian/index.php
@@ -45,52 +45,106 @@
                         <td class='align-top'><?= $row['pengiriman']; ?> <?= $row['ingJenisKemasan']; ?> </td>
                         <td class='align-top'><a href="<?= base_url();?>cetak/surat_perintah_kerja/<?= $row['idBatch'];?>/2/<?= $row['idJP_used'];?>" class="btn btn-warning" data-toggle='tooltip' title='cetak surat perintah kerja' target='blank'><i class="fa fa-print"></i></a></td>
                         <td class='align-top'>blm selesai</td>
-                        <td class='align-top'><a href="" class="btn btn-primary" data-toggle='modal' data-target='#upload-<?= $row['idJP_used'];?>' data-toggle='tooltip' title='Upload pengujian dengan no. admin <?= $noAdm;?>'><i class="fa fa-upload"></i></a></td>
+                        <td class='align-top'>
+                            <?php $this->db->where('idJP_used', $row['idJP_used']) ; ?>
+                            <?php $hasil_pengujian = $this->db->get('hasil_pengujian')->row_array() ; ?>
+                            <?php if($hasil_pengujian) : ?>
+                                <a href="" class="btn btn-success" data-toggle='modal' data-target='#ubah-<?= $row['idJP_used'];?>' data-toggle='tooltip' title='Ubah Hasil Pengujian dengan no. admin <?= $noAdm;?>'><i class="fa fa-edit"></i></a>
+                            <?php else : ?>
+                                <a href="" class="btn btn-primary" data-toggle='modal' data-target='#upload-<?= $row['idJP_used'];?>' data-toggle='tooltip' title='Upload pengujian dengan no. admin <?= $noAdm;?>'><i class="fa fa-upload"></i></a>
+                            <?php endif ; ?>
+                        </td>
                         <!-- note -->
                     </tr>
 
 
-                    <!-- Modal Upload -->
+                    <!-- Modal Upload Simpen Hasil Pengujian -->
                     <div class="modal fade" id="upload-<?= $row['idJP_used'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                    <div class="modal-dialog" role="document">
-                        <div class="modal-content">
-                            <div class="modal-header">
-                                <h5 class="modal-title" id="exampleModalLabel">Upload Hasil Pengujian No. Admin <br> <?= $noAdm ;?></h5>
-                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                                </button>
+                        <div class="modal-dialog" role="document">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <h5 class="modal-title" id="exampleModalLabel">Upload Hasil Pengujian No. Admin <br> <?= $noAdm ;?></h5>
+                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                    </button>
+                                </div>
+
+                                <form action="<?= base_url();?>__pengujian/simpan_hasil_pengujian/<?= $row['idJP_used']; ?>/<?= $row['idBatch'];?>" method="post" enctype="multipart/form-data">
+                                    <div class="modal-body">
+                                        <input type="hidden" name='noAdm' value='<?= $noAdm; ?>'>
+
+                                        <label for="uji_yg_dilakukan">Uji Yang Dilakukan </label>
+                                        <input type="text" name="uji_yg_dilakukan" id="uji_yg_dilakukan" class='form-control'>
+                                        
+                                        <label for="hasil">Hasil Pengujian </label>
+                                        <input type="text" name="hasil" id="hasil" class='form-control'>
+
+                                        <label for="acuan">Acuan </label>
+                                        <input type="text" name="acuan" id="acuan" class='form-control'>
+
+                                        <label for="syarat">Syarat </label>
+                                        <input type="text" name="syarat" id="syarat" class='form-control'>
+
+                                        <label for="tgl_expiry_pengujian">Tanggal Kadaluarsa</label>
+                                        <input type="date" name="tgl_expiry_pengujian" id="tgl_expiry_pengujian" class='form-control'>
+                                        
+                                        <label for="berkas">Upload Hasil Pengujian</label>
+                                        <input type="file" name="berkas" id="berkas" class='form-control'>
+                                        <i class="text-danger">*file pdf,doc,docx</i>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary">Simpan</button>
+                                    </div>
+                                </form>
                             </div>
-
-                            <form action="<?= base_url();?>__pengujian/simpan_hasil_pengujian/<?= $row['idJP_used']; ?>/<?= $row['idBatch'];?>" method="post" enctype="multipart/form-data">
-                                <div class="modal-body">
-                                    <input type="hidden" name='noAdm' value='<?= $noAdm; ?>'>
-
-                                    <label for="uji_yg_dilakukan">Uji Yang Dilakukan </label>
-                                    <input type="text" name="uji_yg_dilakukan" id="uji_yg_dilakukan" class='form-control'>
-                                    
-                                    <label for="hasil">Hasil Pengujian </label>
-                                    <input type="text" name="hasil" id="hasil" class='form-control'>
-
-                                    <label for="acuan">Acuan </label>
-                                    <input type="text" name="acuan" id="acuan" class='form-control'>
-
-                                    <label for="syarat">Syarat </label>
-                                    <input type="text" name="syarat" id="syarat" class='form-control'>
-
-                                    <label for="tgl_expiry_pengujian">Tanggal Kadaluarsa</label>
-                                    <input type="date" name="tgl_expiry_pengujian" id="tgl_expiry_pengujian" class='form-control'>
-                                    
-                                    <label for="berkas">Upload Hasil Pengujian</label>
-                                    <input type="file" name="berkas" id="berkas" class='form-control'>
-                                    <i class="text-danger">*file pdf,doc,docx</i>
-                                </div>
-                                <div class="modal-footer">
-                                    <button type="submit" class="btn btn-primary">Simpan</button>
-                                </div>
-                            </form>
                         </div>
                     </div>
-                    </div>
+
+                    <?php if($hasil_pengujian) : ?>
+                    <!-- Modal ubah Hasil Pengujian -->
+                        <div class="modal fade" id="ubah-<?= $row['idJP_used'];?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Ubah Hasil Pengujian No. Admin <br> <?= $noAdm ;?></h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                        <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+
+                                    <form action="<?= base_url();?>__pengujian/ubah_hasil_pengujian/<?= $hasil_pengujian['id_hasil_pengujian']; ?>/<?= $row['idBatch'];?>" method="post" enctype="multipart/form-data">
+                                        <div class="modal-body">
+                                            <input type="hidden" name='noAdm' value='<?= $noAdm; ?>'>
+
+                                            <label for="uji_yg_dilakukan">Uji Yang Dilakukan </label>
+                                            <input type="text" name="uji_yg_dilakukan" id="uji_yg_dilakukan" class='form-control' value='<?= $hasil_pengujian['uji_yg_dilakukan'];?>'>
+                                            
+                                            <label for="hasil">Hasil Pengujian </label>
+                                            <input type="text" name="hasil" id="hasil" class='form-control' value='<?= $hasil_pengujian['hasil'];?>'>
+
+                                            <label for="acuan">Acuan </label>
+                                            <input type="text" name="acuan" id="acuan" class='form-control' value='<?= $hasil_pengujian['acuan'];?>'>
+
+                                            <label for="syarat">Syarat </label>
+                                            <input type="text" name="syarat" id="syarat" class='form-control' value='<?= $hasil_pengujian['syarat'];?>'>
+
+                                            <label for="tgl_expiry_pengujian">Tanggal Kadaluarsa</label>
+                                            <input type="date" name="tgl_expiry_pengujian" id="tgl_expiry_pengujian" class='form-control' value='<?= $hasil_pengujian['tgl_kadaluarsa_sample'];?>'>
+                                            
+                                            <label for="berkas">Upload Hasil Pengujian</label>
+                                            <input type="file" name="berkas" id="berkas" class='form-control'>
+                                            <i class="text-danger">*file pdf,doc,docx <br> *tambahkan berkas jika ingin ubah berkas hasil pengujian</i>
+
+                                            <label for="">Hasil Pengujian</label>
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="submit" class="btn btn-primary">Simpan</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    <?php endif ; ?>
                 <?php endforeach ; ?>
             </tbody>
         </table>
